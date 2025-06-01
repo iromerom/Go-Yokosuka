2971b1bf7dcafe4ceeea7369183febf60f8b4374
iromerom <ivanirmoyano@gmail.com>
2025-06-01 23:47:44 +0200

    Unity
    Menu Inicio

---
+++ Go!Yokosuka/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// IVAN ROMERO MOYANO //
+
+public class MenuManager : MonoBehaviour
+{
+    public void Update()
+    {
+        // Si pulsamos la tecla "Esc" podemos volver a la escena previa //
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    ///////////////////////////////////////////
+    // Empezar Partida //
+    public void Play()
+    {
+        SceneManager.LoadScene(4);
+
+    }
+   
+    // Acceder al menu de Opciones //
+    public void Options()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    // Acceder la escena de Creditos //
+    public void Credits()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    // Salir del juego //
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Se ha salido del juego");
+    }
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+      public void SP()
+    {
+        SceneManager.LoadScene(3);
+
+    }
+}
