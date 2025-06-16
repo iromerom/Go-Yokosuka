d59fd56b1f85537db3f4759a5a5ed26b4a792711
iromerom <ivanirmoyano@gmail.com>
2025-06-16 15:42:23 +0200

    Comienzo Dobuita Game

--- Go!Yokosuka/Assets/Material/Scripts/MenuManager.cs
+++ Go!Yokosuka/Assets/Material/Scripts/MenuManager.cs
@@ -24,7 +24,7 @@ public class MenuManager : MonoBehaviour
    
 
     ///////////////////////////////////////////
-    ///FUNCIONES DE BOTONES///
+            ///FUNCIONES DE BOTONES///
     ///////////////////////////////////////////
 
     // Empezar Partida //
