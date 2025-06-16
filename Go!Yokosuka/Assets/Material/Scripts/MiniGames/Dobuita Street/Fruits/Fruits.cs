0205c83f2ce4b84fe84a5f3317d0266f5634f609
iromerom <ivanirmoyano@gmail.com>
2025-06-16 17:01:56 +0200

    dobuita fall ingredients

---
+++ Go!Yokosuka/Assets/Material/Scripts/MiniGames/Dobuita Street/Fruits/Fruits.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Audio;
+
+// IVAN ROMERO MOYANO //
+public class Fruits : MonoBehaviour
+{
+    private const int targetY = -20;  // Este es el valor fijo que quieres para Y
+    Vector3 target;  // Esta es la posición final del objeto
+
+    private void Awake()
+    {
+        Reset();  // Llama al método Reset cuando el objeto se despierte
+    }
+
+    private void Reset()
+    {
+        // Obtiene la posición actual del objeto (transform.position)
+        target = transform.position;
+
+        // Cambia solo la componente Y, dejando X y Z igual que antes
+        target = new Vector3(target.x, targetY, target.z);
+    }
+
+    void Update()
+    {
+        MoveDown();  // Llama a MoveDown en cada frame
+    }
+
+    private void MoveDown()
+    {
+        float step = 350f * Time.deltaTime;  // La velocidad del movimiento
+        // Mueve el objeto hacia el objetivo (target) de manera suave
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+    }
+}
